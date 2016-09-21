08232215f57603e16d91ca6f2500b50fbd758b0a
hiromin <hiromin@tesging_server>
2016-09-21 14:36:48 +0900

    20160921-1436:create sub02 and file, update tree.txt

--- changeOwners.sh
+++ changeOwners.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 chown vagrant:vagrant ./.
 chown vagrant:vagrant ./test01.txt
-chown vagrant:vagrant ./test02.txt
-chown vagrant:vagrant ./test003.txt
-chown vagrant:vagrant ./test04.txt
-chown vagrant:vagrant ./changeOwners.sh
 chown vagrant:vagrant ./sub01
 chown vagrant:vagrant ./sub01/subtest01.txt
+chown vagrant:vagrant ./changeOwners.sh
+chown vagrant:vagrant ./test003.txt
+chown vagrant:vagrant ./test02.txt
+chown vagrant:vagrant ./sub02
+chown vagrant:vagrant ./sub02/subtest02.txt
+chown vagrant:vagrant ./tree.txt
