eb7816ef1f935a849b3f175f9ea3606095fb7428
hiromin <hiromin@tesging_server>
2016-09-21 10:40:35 +0900

    20160921-1040:append test04.txt and change createChangedOwner.sh script.

---
+++ changeOwners.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+chown vagrant:vagrant ./.
+chown vagrant:vagrant ./test01.txt
+chown vagrant:vagrant ./test02.txt
+chown vagrant:vagrant ./test003.txt
+chown vagrant:vagrant ./test04.txt
+chown root:root ./changeOwners.sh
