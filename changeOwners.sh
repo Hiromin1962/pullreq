79fd3a88eda9744b3fa899b3b5e5f41320203664
Hiromin1962 <hiromi.okada@gmail.com>
2016-09-23 22:04:42 +0900

    change folder contents.

--- changeOwners.sh
+++ changeOwners.sh
@@ -4,8 +4,8 @@ chown vagrant:vagrant ./test01.txt
 chown vagrant:vagrant ./changeOwners.sh
 chown vagrant:vagrant ./test003.txt
 chown vagrant:vagrant ./test02.txt
-chown vagrant:vagrant ./sub02
-chown vagrant:vagrant ./sub02/subtest02.txt
+
+
 chown vagrant:vagrant ./tree.txt
 chown vagrant:vagrant ./sub03
 chown vagrant:vagrant ./sub03/subtest03.txt
